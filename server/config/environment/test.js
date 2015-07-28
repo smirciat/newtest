fd300a9d280993ceecaf342190e473dde42aedf0
Andy Smircich <andy@smokeybayair.com>
2015-07-28 19:29:19 +0000

    more mongo

--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -5,6 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://heroku_r2bj9pfm:qdcgm69ftg1psopqdcnru84gfu@ds027483.mongolab.com:27483/heroku_r2bj9pfm'//'mongodb://localhost/workspace-test'
+    uri: process.env.MONGOLAB_URI || 'mongodb://localhost/workspace-test'
+    //uri: 'mongodb://heroku_r2bj9pfm:qdcgm69ftg1psopqdcnru84gfu@ds027483.mongolab.com:27483/heroku_r2bj9pfm'//'mongodb://localhost/workspace-test'
   }
 };
