ea9cce4c6962524c501918af4fc623e4d080abe8
Andy Smircich <andy@smokeybayair.com>
2015-07-29 16:49:42 +0000

    revert

--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: process.env.MONGOLAB_URI
+    uri: 'mongodb://heroku_r2bj9pfm:qdcgm69ftg1psopqdcnru84gfu@ds027483.mongolab.com:27483/heroku_r2bj9pfm'
     //uri:'mongodb://localhost/workspace-test'
   }
 };
